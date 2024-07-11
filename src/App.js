7d70cd8d9fe487944a4ef0f2fe72ea345f622202
muznaahmer <muznaahmer@gmail.com>
2024-07-11 17:33:45 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import{Footer,Blog,Possibility,Features,What,Header} from './containers';
-
 import{Cta,Brand,Navbar} from './components';
 
+import './App.css';
 const App = () => {
   return (
     <div className='App '>
